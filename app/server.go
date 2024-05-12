9a289009cea62ccd20ef2c4b545a0b0d29597b3a
Anthony Fiddes <11233666+Anthony-Fiddes@users.noreply.github.com>
2024-05-12 16:59:27 -0700

    Scan start line and return 404 when requset path != /

--- app/server.go
+++ app/server.go
@@ -1,10 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
 	"net"
-	"os"
+	"strings"
 )
 
 type HTTPResponse struct {
@@ -32,16 +33,34 @@ func main() {
 
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
-		log.Print("Failed to bind to port 4221")
-		os.Exit(1)
+		log.Fatal("Failed to bind to port 4221")
 	}
+	defer l.Close()
 
 	conn, err := l.Accept()
 	defer conn.Close()
 	if err != nil {
-		log.Fatal("Error accepting connection: ", err.Error())
+		log.Panic("Error accepting connection: ", err.Error())
 	}
 
-	okResponse := HTTPResponse{"HTTP/1.1", 200, "OK"}
-	conn.Write([]byte(okResponse.String()))
+	scanner := bufio.NewScanner(conn)
+	if err != nil {
+		log.Panic("Error reading from connection")
+	}
+	// don't need to read anything except the start line at this point
+	if !scanner.Scan() {
+		log.Panic("Could not read from connection: ", scanner.Err())
+	}
+
+	// startLine would look like "GET /index.html HTTP/1.1"
+	startLine := scanner.Text()
+	sl := strings.Split(startLine, " ")
+	requestPath := sl[1]
+	if requestPath != "/" {
+		response := HTTPResponse{"HTTP/1.1", 404, "Not Found"}
+		conn.Write([]byte(response.String()))
+	} else {
+		okResponse := HTTPResponse{"HTTP/1.1", 200, "OK"}
+		conn.Write([]byte(okResponse.String()))
+	}
 }
