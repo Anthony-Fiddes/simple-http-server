c0b28d122b28652037397fca4daaf226bdd67f01
Anthony Fiddes <11233666+Anthony-Fiddes@users.noreply.github.com>
2024-05-12 16:07:32 -0700

    Respond to one request

--- app/server.go
+++ app/server.go
@@ -2,23 +2,46 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net"
 	"os"
 )
 
+type HTTPResponse struct {
+	version string
+	status  int
+	reason  string
+}
+
+func (h HTTPResponse) String() string {
+	if h.version == "" {
+		h.version = "HTTP/1.1"
+	}
+
+	result := fmt.Sprintf("%s %d", h.version, h.status)
+	if h.reason != "" {
+		result = fmt.Sprintf("%s %s", result, h.reason)
+	}
+	result += "\r\n\r\n"
+	return result
+}
+
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
+	log.Print("Logs from your program will appear here!")
 
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
-		fmt.Println("Failed to bind to port 4221")
+		log.Print("Failed to bind to port 4221")
 		os.Exit(1)
 	}
 
-	_, err = l.Accept()
+	conn, err := l.Accept()
+	defer conn.Close()
 	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+		log.Fatal("Error accepting connection: ", err.Error())
 	}
+
+	okResponse := HTTPResponse{"HTTP/1.1", 200, "OK"}
+	conn.Write([]byte(okResponse.String()))
 }
